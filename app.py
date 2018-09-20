fda12cc2f3b7e6aac1af706365aac9bae44d800b
Louise Deconinck <louise.deconinck@ugent.be>
2018-09-20 18:06:47 +0200

    ?

--- app.py
+++ app.py
@@ -1,45 +1,50 @@
-from flask import Flask
+from flask import Flask, send_from_directory, send_file
 from flask import request
 from flask import session
+
 from datetime import datetime
 
 app = Flask(__name__)
-app.secret_key = b'super_secret_stuff'
+app.secret_key = b'super_secret_stuff2'
+
+scores = dict()
+endtime = 0
 
 
 @app.route('/')
 def hello_world():
-    scores = {"noone": 5}
-    session['scores'] = scores
     return 'Hello World!'
 
 
 @app.route('/scores', methods=["GET"])
 def get_scores():
-    scores = session['scores']
     sorted_scores = sorted(scores.items(), key=lambda e: -e[1])[:10]
-    return str(sorted_scores)
+    sstring = "<table>"
+    for score in sorted_scores:
+        sstring += "<tr><td>" + str(score[0])
+        sstring += "</td><td>" + str(score[1]) + "</td>"
+    sstring += "</table>"
+    return sstring
 
 
 @app.route('/scores', methods=["POST"])
 def post_score():
-    scores = session['scores']
     new_score = request.form['score']
     name = request.form['name']
-    scores[name] = new_score
+    scores[name] = int(new_score)
     session['scores'] = scores
-    return 0
+    return '', 201
 
 
 @app.route('/start', methods=["POST"])
 def start_timer():
-    app.config['ENDTIME'] = datetime.now()
+    endtime = datetime.now()
     return 0
 
 
 @app.route('/game', methods=["GET"])
 def get_game():
-    return 0
+    return send_file('data/index.html', as_attachment=True)
 
 
 if __name__ == '__main__':
